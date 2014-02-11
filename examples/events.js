93993dfe522192506386ef9822cb06ceeae928f7
TJ Holowaychuk <tj@vision-media.ca>
2014-02-10 17:53:45 -0800

    add example

---
+++ examples/events.js
@@ -0,0 +1,27 @@
+
+/**
+ * Module dependencies.
+ */
+
+var request = require('superagent');
+
+setInterval(function(){
+  console.log('... sending');
+
+  request
+  .post('http://localhost:3000')
+  .send({
+    timestamp: Date.now(),
+    hostname: 'api-1',
+    type: 'login',
+    level: 'info',
+    message: {
+      user: {
+        name: {
+          first: 'Tobi',
+          last: 'Ferret'
+        }
+      }
+    }
+  });
+}, 500);
